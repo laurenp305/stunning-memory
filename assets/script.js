6ef6361d485c9f5b504ce9a0024151e19ffddc74
laurenp305 <lamor1800@gmail.com>
2022-07-04 16:50:06 -0400

    edited info box buttons button styling

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 // getting all required elements
 const start_button = document.querySelector(".start-button");
 const info_box = document.querySelector(".info-box");
-const exit_button = info-box.querySelector(".buttons .quit");
-const continue_button = info-box.querySelector(".buttons .restart");
+const exit_button = info_box.querySelector(".buttons .quit");
+const continue_button = info_box.querySelector(".buttons .restart");
 
 //if Start Quiz Button Clicked
 start_button.onclick = ()=>{
-    info-box.classList.add("activeInfo")
+    info_box.classList.add("activeInfo") //show the info box
 }
 
-//if Start Quiz Button Clicked
-start_button.onclick = ()=>{
-    info-box.classList.add("activeInfo")
+//if Exit Button Clicked
+exit_button.onclick = ()=>{
+    info_box.classList.remove("activeInfo")//hide the info box
 }
