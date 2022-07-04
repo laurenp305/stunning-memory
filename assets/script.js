86916c49331b910d1eb44dd0bbdd23d16020e5b2
laurenp305 <lamor1800@gmail.com>
2022-07-04 16:47:40 -0400

    added info box active styling and start button function

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,14 @@
 const start_button = document.querySelector(".start-button");
 const info_box = document.querySelector(".info-box");
 const exit_button = info-box.querySelector(".buttons .quit");
-const continue_button = document.querySelector(".buttons .restart");
+const continue_button = info-box.querySelector(".buttons .restart");
 
-//
+//if Start Quiz Button Clicked
+start_button.onclick = ()=>{
+    info-box.classList.add("activeInfo")
+}
+
+//if Start Quiz Button Clicked
+start_button.onclick = ()=>{
+    info-box.classList.add("activeInfo")
+}
