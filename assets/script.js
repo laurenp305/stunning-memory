8a8b2bffaa54f438563bda0db22d83d7d4222c67
laurenp305 <lamor1800@gmail.com>
2022-07-04 17:06:35 -0400

    created javascript file for questions

--- assets/script.js
+++ assets/script.js
@@ -7,17 +7,18 @@ const quiz_button = document.querySelector(".quiz_box");
 
 //if Start Quiz Button Clicked
 start_button.onclick = ()=>{
-    info_box.classList.add("activeInfo") //show the info box
+    info_box.classList.add("activeInfo"); //show the info box
 }
 
 //if Exit Button Clicked
 exit_button.onclick = ()=>{
-    info_box.classList.remove("activeInfo")//hide the info box
+    info_box.classList.remove("activeInfo");//hide the info box
 }
 
 //if Continue Button Clicked
 continue_button.onclick = ()=>{
-    info_box.classList.remove("activeInfo")//hide the info box
-    quiz_box.classList.add("activeQuiz")//show the quiz box
+    info_box.classList.remove("activeInfo");//hide the info box
+    quiz_box.classList.add("activeQuiz");//show the quiz box
 }
+
 
