dfa866adbeae20853d81c921c4bbf511a394a881
laurenp305 <lamor1800@gmail.com>
2022-07-04 17:02:17 -0400

    changed div labeling

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 // getting all required elements
-const start_button = document.querySelector(".start-button");
-const info_box = document.querySelector(".info-box");
+const start_button = document.querySelector(".start_button");
+const info_box = document.querySelector(".info_box");
 const exit_button = info_box.querySelector(".buttons .quit");
 const continue_button = info_box.querySelector(".buttons .restart");
-const quiz_button = info_box.querySelector(".quiz_box");
+const quiz_button = document.querySelector(".quiz_box");
 
 //if Start Quiz Button Clicked
 start_button.onclick = ()=>{
@@ -18,6 +18,6 @@ exit_button.onclick = ()=>{
 //if Continue Button Clicked
 continue_button.onclick = ()=>{
     info_box.classList.remove("activeInfo")//hide the info box
-    quiz_box.classList.remove("activeQuiz")//show the quiz box
+    quiz_box.classList.add("activeQuiz")//show the quiz box
 }
 
