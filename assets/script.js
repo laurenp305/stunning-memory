03daff6ed401b0ad3aac486efa319673c6806bda
laurenp305 <lamor1800@gmail.com>
2022-07-04 16:24:31 -0400

    added variables to javascript

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// getting all required elements
+const start_button = document.querySelector(".start-button");
+const info_box = document.querySelector(".info-box");
+const exit_button = info-box.querySelector(".buttons .quit");
+const continue_button = document.querySelector(".buttons .restart");
+
+//
