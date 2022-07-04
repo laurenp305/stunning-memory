dd4f3eb86c479263beb14fac62d4490db4a9e438
laurenp305 <lamor1800@gmail.com>
2022-07-04 17:31:12 -0400

    added functions for questions and answers in js

--- assets/script.js
+++ assets/script.js
@@ -19,15 +19,21 @@ exit_button.onclick = ()=>{
 continue_button.onclick = ()=>{
     info_box.classList.remove("activeInfo");//hide the info box
     quiz_box.classList.add("activeQuiz");//show the quiz box
-    showQuestions();
+    showQuestions(0);
 }
 
 let que_count = 0;
 
 //getting questions and options from array in other js sheet
-function showQuestions(){
+function showQuestions(index){
     const que_text = document.querySelector(".que_text");
-    let que_tag = '<span>'+ questions[1].question + '</span>';
+    const option_list = document.querySelector(".option_list");
+    let que_tag = '<span>'+ questions[index].question + '</span>';
+    let option_tag = '<div class="option">' + questions[index].options[0] +'<span></span></div>'
+                    + '<div class="option"><span></span></div>';
+                    + '<div class="option"><span></span></div>';
+                    + '<div class="option"><span></span></div>';
     que_text.innerHTML = que_tag; 
+    option_list.innerHTML = option_tag;
 }
 
