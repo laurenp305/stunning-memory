62a276127ce2af351007489f00079d3310425449
laurenp305 <lamor1800@gmail.com>
2022-07-04 16:54:59 -0400

    added constants and button functions

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ const start_button = document.querySelector(".start-button");
 const info_box = document.querySelector(".info-box");
 const exit_button = info_box.querySelector(".buttons .quit");
 const continue_button = info_box.querySelector(".buttons .restart");
+const quiz_button = info_box.querySelector(".quiz_box");
 
 //if Start Quiz Button Clicked
 start_button.onclick = ()=>{
@@ -13,3 +14,10 @@ start_button.onclick = ()=>{
 exit_button.onclick = ()=>{
     info_box.classList.remove("activeInfo")//hide the info box
 }
+
+//if Continue Button Clicked
+continue_button.onclick = ()=>{
+    info_box.classList.remove("activeInfo")//hide the info box
+    quiz_box.classList.remove("activeQuiz")//show the quiz box
+}
+
