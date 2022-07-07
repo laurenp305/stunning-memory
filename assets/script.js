0344f9ac8a3c1b5a2a3e966dfdfd549fbe5552f7
laurenp305 <lamor1800@gmail.com>
2022-07-07 18:12:01 -0400

    fixed javascript error in lines 57-70

--- assets/script.js
+++ assets/script.js
@@ -54,16 +54,17 @@ next_button.onclick = ()=>{
 }
 
 //getting questions and options from array 
-function showQuestions(index){
+function showQuetions(index){
     const que_text = document.querySelector(".que_text");
-    const option_list = document.querySelector(".option_list");
-    let que_tag = '<span>'+ questions[index].question + '</span>';
-    let option_tag = '<div class="option">' + questions[index].options[0] + '<span></span></div>';
-                    + '<div class="option">+ questions[index].options[1] + '<span></span></div>'
-                    + '<div class="option">+ questions[index].options[2] + '<span></span></div>'
-                    + '<div class="option">+ questions[index].options[3] + '<span></span></div>';
-    que_text.innerHTML = que_tag; 
-    option_list.innerHTML = option_tag;
+    //creating a new span and div tag for question and option and passing the value using array index
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
+    option_list.innerHTML = option_tag; //adding new div tag inside option_tag
+    
     const option = option_list.querySelectorAll(".option");
     for (let i = 0; i < option.length; i++) {
         option[i].setAttribute("onclick", "optionSelected(this)")
